594a613641a236c3382c0ce516e6dacf163de04f
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2023-04-23 23:07:37 +0100

    face detection box

--- src/components/FaceDetection.js
+++ src/components/FaceDetection.js
@@ -4,22 +4,24 @@ import "./FaceRecognition.css";
 const FaceDetection = ({ imageUrl, box }) => {
   return (
     <div className="flex justify-center mt-1">
-      <div
-        className="bounding-box"
-        style={{
-          top: box.topRow,
-          bottom: box.bottomRow,
-          right: box.rightCol,
-          left: box.leftCol,
-        }}
-      ></div>
-      <img
-        id="inputimage"
-        src={imageUrl}
-        alt="img"
-        width="500px"
-        height="auto"
-      />
+      <div className=" absolute mt-2">
+        <img
+          id="inputimage"
+          src={imageUrl}
+          alt="img"
+          width="500px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            bottom: box.bottomRow,
+            right: box.rightCol,
+            left: box.leftCol,
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
